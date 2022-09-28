19ccb52793025d96fcfbaa54dcb0653d40cde57d
Melanie <emelanie-rau@outlook.com>
2022-09-28 17:04:52 +0100

    test

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApiUrl).then(response => {
+    fetch(localfunctionApi).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
