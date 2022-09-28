aa3deeedd4f0062ea6d4e8424135b0c244d5adfe
Melanie <emelanie-rau@outlook.com>
2022-09-28 16:07:40 +0100

    Viewcounter locally running now

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
-});
+})
 
-
-const localApi = 'http://localhost:7071/api/GetResumeCounter';
-/*const functionApi = 'https://getresumecounterfunctionapp.azurewebsites.net/api/GetResumeCounter?code=jp4rx3wuLEzaXvSFeBKaq08RGiVCwRtpAA1Cdxdc15c9ZGjM79tWjg=='; 
-*/
+/* const functionApiUrl = 'https://getvisitorcounter.azurewebsites.net/api/GetVisitorCounter'; */
+const functionApiUrl = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi)
-    .then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
-    })
-    .then(response => {
+    }).then(response =>{
         console.log("Website called function API.");
-        count = response.count;
-        document.getElementById('counter').innerText = count;
-    }).catch(function(error) {
+        count =  response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
         console.log(error);
-      });
+    });
     return count;
 }
